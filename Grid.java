2b6649710ba9a36164781bb51fa796acd23f6487
Oscar Cordero <oscar101019@gmail.com>
2016-02-02 14:02:52 -0600

    Importaciones y variables del proyecto

---
+++ Grid.java
@@ -0,0 +1,14 @@
+import javax.swing.JButton; //importaciones
+import javax.swing.JPanel;
+import java.awt.GridLayout;
+import javax.swing.JFrame;
+
+
+    public class Grid{
+
+    JFrame frame;
+    JPanel ventana;
+    JButton bt0 ,bt1, bt2, bt3, bt4, bt5;
+
+    }  
+					
