808f8fe34d259e74df08092d697fc18ffb78926d
Emanuel Cardenas <emanuel_cardenas@hotmail.com>
2016-02-02 16:36:36 -0600

    Grid.java

--- Grid.java
+++ Grid.java
@@ -18,10 +18,22 @@ import javax.swing.JFrame;
 
      void Panel(){
       ventana=new JPanel();
-      ventana.setLayout(new GridLayout(3,2,10,10);
+      ventana.setLayout(new GridLayout(3,2,10,10));
+        bt0=new JButton("0");
+      bt1=new JButton("1");
+      bt2=new JButton("2");
+      bt3=new JButton("3");
+      bt4=new JButton("4");
+      bt5=new JButton("5");
+      
+     ventana.add(bt0);
+     ventana.add(bt1);
+     ventana.add(bt2);
+     ventana.add(bt3);
+     ventana.add(bt4);
+     ventana.add(bt5);
 
-
-     }
+ }
 
 
     }  
