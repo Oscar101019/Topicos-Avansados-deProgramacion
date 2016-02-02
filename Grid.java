358e8c603403058539c2147c54bb7d4d0b8afea0
Dylan Salvador <ds232009@hotmail.com>
2016-02-02 15:13:28 -0600

    Grid.java

--- Grid.java
+++ Grid.java
@@ -10,5 +10,19 @@ import javax.swing.JFrame;
     JPanel ventana;
     JButton bt0 ,bt1, bt2, bt3, bt4, bt5;
 
+     public Grid(){
+      Panel();
+      construyeVentana();
+
+     }
+
+     void Panel(){
+      ventana=new JPanel();
+      ventana.setLayout(new GridLayout(3,2,10,10);
+
+
+     }
+
+
     }  
 					
